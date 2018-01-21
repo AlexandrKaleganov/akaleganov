80362ea077b33078b6d78911134476bb908d8b1f
AlexandrKaleganov <alexmur07@mail.ru>
2018-01-21 21:28:35 +0700

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,15 @@
+/**
+*Package for calculate task.
+*
+*@autor Alexandr Kaleganov (alexmur07@mail.ru)
+*@version 1.0
+*@since 21.01.2018
+*/
 package ru.job4j;
 
 public class Calculate{
      public static void main (String[] args){
-	System.out.println("Hello world");//"вывод строки в консоль Hello World"
+	System.out.println("Hello world");//"вывод строки в консоль "Hello World"
     } 
 }
     
