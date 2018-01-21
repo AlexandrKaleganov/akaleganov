2f59339b0f2bbc1bd0f542a6d0e501f5d4e58da4
AlexandrKaleganov <alexmur07@mail.ru>
2018-01-21 22:18:11 +0700

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,4 @@
+package ru.job4j;
 /**
 *Package for calculate task.
 *
@@ -5,11 +6,17 @@
 *@version 1.0
 *@since 21.01.2018
 */
-package ru.job4j;
-
 public class Calculate{
      public static void main (String[] args){
-	System.out.println("Hello world");//"вывод строки в консоль "Hello World"
+	System.out.println("Hello world");
     } 
+/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
     
