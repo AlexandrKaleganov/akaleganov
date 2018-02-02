0b6f4325dc948d4f5145a297f0e013257a322457
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-02 11:55:14 +0700

    5.2. Перевернуть массив. [#4441]

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -8,7 +8,7 @@ public class TurnTest {
     @Test
     public void testirovanieArrayTurnFOReach() {
         int[] excepted = new int[]{3, 2, 1};
-        int[] arr123 = new int[]{1,2,3};
+        int[] arr123 = new int[]{1, 2, 3};
 
         int[] intarray = new Turn().backArray(arr123);
         assertThat(intarray, is(excepted));
