56acaada20190a75923b193e519456e0f6ca26a9
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-03 19:47:17 +0700

    123

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ *
+ */
+public class BubbleSort {
+    public int[] sort(int[] array){
+        int[]result=null;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                //if (array[index]<array[])
+            }
+            if (array[i]>array[i+1]) {
+                //array[i+1];
+            }
+
+        }
+        return result;
+    }
+}
--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -8,7 +8,7 @@ package ru.job4j.array;
 public class FindLoop {
     public int indexOf(int[]armass, int el) {
         int rsl = -1;
-        for (int i = 0; i < armass.length ; i++) {
+        for (int i = 0; i < armass.length; i++) {
             if (armass[i] == el) {
                 rsl = i;       //присваиваем индекс элемента      с каждым кругом интдекс увеличиваем на 1
             }
