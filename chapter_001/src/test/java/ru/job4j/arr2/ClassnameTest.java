cc90ad90352ec2ba5ca81503bcc4325967b90568
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-11 14:56:22 +0700

    модификатор доступа статик

--- chapter_001/src/test/java/ru/job4j/arr2/ClassnameTest.java
+++ chapter_001/src/test/java/ru/job4j/arr2/ClassnameTest.java
@@ -4,14 +4,14 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 public class ClassnameTest {
     @Test
-    public void TestPeredavaimogoMassiva(){
+    public void testPeredavaimogoMassiva() {
 
 
         double[][] temp = new double[3][4];
         temp[0] = Classname.methodName1();
         temp[1] = Classname.methodName2();
         temp[2] = Classname.methodName3();
-        double[][] expected = {{2.0, 4.0, 6.0, 8.0}, {2.0, 4.0, 6.0, 8.0},{2.0, 4.0, 6.0, 8.0}};
+        double[][] expected = {{2.0, 4.0, 6.0, 8.0}, {2.0, 4.0, 6.0, 8.0}, {2.0, 4.0, 6.0, 8.0}};
         assertThat(temp, is(expected));
     }
 
