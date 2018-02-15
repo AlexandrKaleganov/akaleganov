4f9b323a53738a3f300e5932753b5b52b9a9b973
AdminSec <alexmur07@mail.ru>
2018-02-15 16:57:36 +0700

    два массива перекидать в один отсортированный[#41416]

--- chapter_001/src/main/java/ru/job4j/array/ArraysX2Sort.java
+++ chapter_001/src/main/java/ru/job4j/array/ArraysX2Sort.java
@@ -7,19 +7,18 @@ package ru.job4j.array;
  * i+=2 - пойдём с шагом в два элемента, т.к. мы будем сразу два элемента запихивать в новый массив
  */
 public class ArraysX2Sort {
-    public static int[] SsortirovkaMassiva(int[]ar1, int[]ar2){
-        int[]ar3 = new int[ar1.length+ar2.length];
-        int k =0;
-        for (int i = 0; i < ar3.length - 1; i+=2) {
-            if (ar1[k]<ar2[k]){
+    public static int[] ssortirovkaMassivavodin(int[]ar1, int[]ar2) {
+        int[]ar3 = new int[ar1.length + ar2.length];
+        int k = 0;
+        for (int i = 0; i < ar3.length - 1; i += 2) {
+            if (ar1[k] < ar2[k]) {
                 ar3[i] = ar1[k];
-                ar3[i+1]= ar2[k];
+                ar3[i + 1] = ar2[k];
             } else {
                 ar3[i] = ar2[k];
-                ar3[i+1]= ar1[k];
-            } k++;
-
-        }
-        return ar3;
+                ar3[i + 1] = ar1[k];
+            }
+            k++;
+        } return ar3;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArraysX2SortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArraysX2SortTest.java
@@ -4,11 +4,11 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 public class ArraysX2SortTest {
     @Test
-    public void testirovanieArrayDuplicateTEST() {
-        int[] array = {1,4,5};
-        int[] array2 = {2,3,6};
-        int[] array3 = ArraysX2Sort.SsortirovkaMassiva(array,array2);
-        int[] expected = {1,2,3,4,5,6};
+    public void testirovanieArraMASSIVtest() {
+        int[] array = {1, 4, 5};
+        int[] array2 = {2, 3, 6};
+        int[] array3 = ArraysX2Sort.ssortirovkaMassivavodin(array, array2);
+        int[] expected = {1, 2, 3, 4, 5, 6};
         assertThat(array3, is(expected));
     }
 }
