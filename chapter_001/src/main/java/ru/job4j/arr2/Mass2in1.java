3948a1e8c2658b287a771948008ccb102c8a7d17
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-17 22:20:30 +0700

    массив два в один

---
+++ chapter_001/src/main/java/ru/job4j/arr2/Mass2in1.java
@@ -0,0 +1,31 @@
+package ru.job4j.arr2;
+
+/**
+ * вернулся к первоначальному решению
+ * даже боялся лишние строки писать ))
+ */
+public class Mass2in1 {
+
+    public static int[] ssortirovkaMassivavodin(int[]ar1, int[]ar2) {
+        int[]ar3 = new int[ar1.length + ar2.length];
+        int k = 0;
+        int n = 0;
+        for (int i = 0; i < ar3.length; i++) {
+            if (k < ar1.length && n < ar2.length) {
+                if (ar1[k] <= ar2[n]) {
+                    ar3[i] = ar1[k];
+                    k++;
+                } else {
+                    ar3[i] = ar2[n];
+                    n++;
+                }
+            } else if (k < ar1.length && n >= ar2.length) {
+                ar3[i] = ar1[k];
+                k++;
+            } else if (n < ar2.length && k >= ar1.length) {
+                ar3[i] = ar2[n];
+                n++;
+            }
+        } return ar3;
+    }
+}
