dc643d312240964518f01568ec75672d99e858cb
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-17 22:19:37 +0700

    массив два в один

--- chapter_001/src/test/java/ru/job4j/arr2/Mass2in1Test.java
+++ chapter_001/src/test/java/ru/job4j/arr2/Mass2in1Test.java
@@ -6,10 +6,10 @@ public class Mass2in1Test {
 
     @Test
     public void testirovanieArraMASSIVtest() {
-        int[] array = {1, 4, 5};
+        int[] array = {1, 4, 5, 260};
         int[] array2 = {2, 200, 6300};
         int[] array3 = Mass2in1.ssortirovkaMassivavodin(array, array2);
-        int[] expected = {1, 2, 4, 5, 200, 6300};
+        int[] expected = {1, 2, 4, 5, 200, 260, 6300};
         assertThat(array3, is(expected));
     }
 }
