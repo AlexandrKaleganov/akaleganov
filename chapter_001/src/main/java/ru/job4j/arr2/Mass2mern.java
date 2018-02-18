f337bda7501720c48ba7149feb0117a9da7128b6
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-19 01:57:52 +0700

    1

---
+++ chapter_001/src/main/java/ru/job4j/arr2/Mass2mern.java
@@ -0,0 +1,32 @@
+package ru.job4j.arr2;
+
+public class Mass2mern {
+
+    public static void Arrmass2in1(int arr[][]) {
+        int l = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[1].length; j++) {
+                if (arr[i][j] % 2 > 0){
+                    l++;
+                }
+            }
+        }
+        int k = 0;
+        int n = 0;
+        int[] arr1 = new int[l];
+        int[] arr2 = new int[arr.length * arr[1].length - l];
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[1].length; j++) {
+                if (arr[i][j]%2 > 0){
+                    arr1[k] = arr[i][j];
+                    System.out.println("arr1 НЕ чётный " + k + " = " + arr1[k]);
+                     k++;
+                    } else {
+                    arr2[n] = arr[i][j];
+                    System.out.println("arr2 Чётный " + n + " = " + arr2[n]);
+                    n++;
+                }
+            }
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/arr2/Mass2mernTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.arr2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Mass2mernTest {
+    @Test
+    public void testirovaniedumern() {
+        int[][] arr = new int[][]{
+                {1, 2},
+                {3, 4}};
+        Mass2mern.Arrmass2in1(arr);
+    }
+}
