43c66180cf2cab0cda1c7a4cbb5f862f584495ff
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-18 21:40:17 +0700

    1. Реализация профессий в коде [#6837]

---
+++ chapter_002/src/main/java/ru/oop/prof/dopclass/Direction.java
@@ -0,0 +1,9 @@
+package ru.oop.prof.dopclass;
+
+/**
+ * объекты направлений иженера,
+ * механик,  it, или инженер по лифтам
+ */
+public enum Direction {
+    MECHANIC, IT, ELEVATOR_ENGINEER;
+}
---
+++ chapter_002/src/main/java/ru/oop/prof/dopclass/Lift.java
@@ -0,0 +1,10 @@
+package ru.oop.prof.dopclass;
+
+/**
+ * класс лифты
+ */
+public class Lift extends Equipment {
+    public Lift(String model, int garantia, int lifetime, Rabsost rabsost) {
+        super(model, garantia, lifetime, rabsost);
+    }
+}
