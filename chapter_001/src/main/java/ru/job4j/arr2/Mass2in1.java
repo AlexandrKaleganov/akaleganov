810f67193e9f649c1bbda4a64777d9950636be6f
AlexandrKaleganov <alexmur07@mail.ru>
2018-02-21 21:35:38 +0700

    1

--- chapter_001/src/main/java/ru/job4j/arr2/Mass2in1.java
+++ chapter_001/src/main/java/ru/job4j/arr2/Mass2in1.java
@@ -13,18 +13,14 @@ public class Mass2in1 {
         for (int i = 0; i < ar3.length; i++) {
             if (k < ar1.length && n < ar2.length) {
                 if (ar1[k] <= ar2[n]) {
-                    ar3[i] = ar1[k];
-                    k++;
+                    ar3[i] = ar1[k++];
                 } else {
-                    ar3[i] = ar2[n];
-                    n++;
+                    ar3[i] = ar2[n++];
                 }
             } else if (k < ar1.length && n >= ar2.length) {
-                ar3[i] = ar1[k];
-                k++;
+                ar3[i] = ar1[k++];
             } else if (n < ar2.length && k >= ar1.length) {
-                ar3[i] = ar2[n];
-                n++;
+                ar3[i] = ar2[n++];
             }
         } return ar3;
     }
