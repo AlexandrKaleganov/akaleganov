9a349ede17f7d5912c70c8a91f4dcbd0fc355865
AlexandrKaleganov <alexmur07@mail.ru>
2018-03-20 13:13:50 +0700

    ТЕстирование Абстрактных классов

---
+++ chapter_002/src/main/java/ru/oop/privatTest/Human.java
@@ -0,0 +1,13 @@
+package ru.oop.privatTest;
+
+abstract class Human {
+    private String name;
+    private int rost;
+
+    public Human(String name, int rost){
+        this.name = name;
+        this.rost = rost;
+
+    }
+    abstract void say();
+}
