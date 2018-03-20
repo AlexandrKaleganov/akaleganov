14dacd5d1fbaf04e6009de8794d4b7f1067d10dd
AlexandrKaleganov <alexmur07@mail.ru>
2018-03-20 23:15:09 +0700

    Перевернуть массив

--- chapter_001/src/main/java/ru/job4j/array/Massrevers.java
+++ chapter_001/src/main/java/ru/job4j/array/Massrevers.java
@@ -6,14 +6,15 @@ package ru.job4j.array;
 import java.util.Scanner;
 
 public class Massrevers {
-    static void returnReversArray(int[] arr){
+    static void returnReversArray(int[] arr) {
         int temp;
-        for (int i = arr.length-1, j = 0; i >=arr.length/2 ; i--,j++) {
+        for (int i = arr.length - 1, j = 0; i >= arr.length / 2; i--, j++) {
             temp = arr[j];
             arr[j] = arr[i];
             arr[i] = temp;
         }
     }
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -22,16 +23,20 @@ public class Massrevers {
 
         int[] arr = new int[sizeArr];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random()*20-5);
+            arr[i] = (int) (Math.random() * 20 - 5);
         }
 
         System.out.print("Первоначальные элементы массива: ");
-        for(int key : arr){ System.out.print(key+" "); }
+        for (int key : arr) {
+            System.out.print(key + " ");
+        }
 
 
         System.out.print("\nПереработонные элементы массива: ");
         returnReversArray(arr);
-        for(int key : arr){ System.out.print(key+" "); }
+        for (int key : arr) {
+            System.out.print(key + " ");
+        }
     }
 }
 
