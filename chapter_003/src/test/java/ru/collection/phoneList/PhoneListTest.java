40b02e755f169443936dab88532c5795ac2d877b
AlexandrKaleganov <alexmur07@mail.ru>
2018-03-22 22:09:43 +0700

    Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_003/src/test/java/ru/collection/phoneList/PhoneListTest.java
@@ -0,0 +1,18 @@
+package ru.collection.phoneList;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneListTest {
+    @Test
+    public void whenFindByName() {
+        PhoneList phones = new PhoneList();
+        phones.addAbonent(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.search("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+}
