7936fd87d42a0132d6b835f06bc4d082ff7ad9a5
AlexandrKaleganov <alexmur07@mail.ru>
2018-03-26 01:43:55 +0700

    БАнкомат ДАющий сдачу

--- chapter_002/src/main/java/ru/oop/bankomat/Bankomat.java
+++ chapter_002/src/main/java/ru/oop/bankomat/Bankomat.java
@@ -7,18 +7,17 @@ public class Bankomat {
 
     public int[] changes(int value, int price) {
         int[] zdacha = new int[]{1, 2, 5, 10};
-        int k = value - price;
         int[] result = new int[30];
-        int sdacha_result = 0;
+        int temp = 0;
         int j = 0;
-        if (k > 0) {
+        if (value - price > 0) {
             for (int i = zdacha.length - 1; i >= 0; i--) {
-                while (k - (sdacha_result + zdacha[i]) >= 0) {
-                    sdacha_result += zdacha[i];
+                while (value - price - (temp + zdacha[i]) >= 0) {
+                    temp += zdacha[i];
                     result[j++] = zdacha[i];
                 }
             }
-        } else if (k < 0) {
+        } else if (value - price < 0) {
             result[0] = value;
             System.out.println("недостаточно денег, заберите вашу купюру обратно");
         }
