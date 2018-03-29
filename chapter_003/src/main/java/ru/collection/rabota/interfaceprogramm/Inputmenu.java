b5b6bd78dfbef67c1c4aea76a7a209ab028c8f39
AlexandrKaleganov <alexmur07@mail.ru>
2018-03-29 15:47:26 +0700

    Чтение файла и вывод в консоль по буковке

--- chapter_003/src/main/java/ru/collection/rabota/interfaceprogramm/Inputmenu.java
+++
@@ -1,8 +0,0 @@
-package ru.collection.rabota.interfaceprogramm;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-
-public interface Inputmenu {
-    public String input(String command) ;
-}
