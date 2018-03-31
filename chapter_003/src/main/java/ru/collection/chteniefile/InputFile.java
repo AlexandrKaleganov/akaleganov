278539efdfc712cb71d8ad132af609b8cc261eb9
AlexandrKaleganov <alexmur07@mail.ru>
2018-03-31 09:48:09 +0700

    исправления

--- chapter_003/src/main/java/ru/collection/chteniefile/InputFile.java
+++ chapter_003/src/main/java/ru/collection/chteniefile/InputFile.java
@@ -9,13 +9,15 @@ import java.util.Scanner;
 
 
 public class InputFile implements Inputmenu {
-Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+
     @Override
     public String input(String command) {
-        if (command!=null){
+        if (command != null) {
             System.out.println(command);
         } else {
-        System.out.println("укажите путь к файлу, паример \"file//google.csv\"");}
+            System.out.println("укажите путь к файлу, паример \"file//google.csv\"");
+        }
         return scanner.nextLine();
     }
 }
